a4909c871941f5b827f37db4c13e21e0056bd229
badolakiran96 <kiran.kumar@hachiweb.com>
2021-11-29 22:49:15 +0530

    Add command description

--- bootloader.asm
+++ bootloader.asm
@@ -14,10 +14,10 @@ PrintString:
     mov ah, 0x0e ;magic value
     .for:
     cmp [bx], byte 0
-    je .exit
+    je .exit   ;Jump if cmp is equal
         mov al, [bx]
         int 0x10
-        inc bx  
+        inc bx   ;The INC instruction is used for incrementing an operand by one. It works on a single operand that can be either in a register or in memory.
         jmp .for
     .exit:
     ret
